f1cc9b9b1d26c6f02f7a28fa59e7071fff84ba92
Connor Low <clow2@my.gcu.edu>
2020-02-29 13:10:38 -0800

    Added Enpoint creator

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,26 @@ export * from './client';
 export { default as tysonClientFactory } from './client'
 import createFactory, { RequestInitializerFactory, ResponseMiddleware } from './client';
 
-export default <T>(rootURL: string, init: RequestInitializerFactory, middleware: ResponseMiddleware<T>) => {
+const createEndpoint = <T>(rootURL: string, init: RequestInitializerFactory, middleware: ResponseMiddleware<T>) => {
     const sendRequest = createFactory(rootURL, init, middleware);
     return {
-        createGet: <T>(query: T, options?: RequestInit) => sendRequest(rootURL, 'GET', query, options),
+        create: {
+            get:
+                <TR, TB extends T>(options?: RequestInit) => (query: TR) => sendRequest<TR, TB>(rootURL, 'GET', query, options),
+            pathedGet:
+                <TR, TB extends T>(options?: RequestInit) => (id: number | string) => sendRequest<TR, TB>(`${rootURL}/${id}`, 'GET', undefined, options),
+            delete:
+                <TR, TB extends T>(options?: RequestInit) => (body: TR) => sendRequest<TR, TB>(rootURL, 'GET', body, options),
+            pathedDelete:
+                <TR, TB extends T>(options?: RequestInit) => (id: number | string, body?: TR) => sendRequest<TR, TB>(`${rootURL}/${id}`, 'DELETE', body, options),
+            post:
+                <TR, TB extends T>(options?: RequestInit) => (body: TR) => sendRequest<TR, TB>(rootURL, 'POST', body, options),
+            put:
+                <TR, TB extends T>(options?: RequestInit) => (body: TR) => sendRequest<TR, TB>(rootURL, 'PUT', body, options),
+            patch:
+                <TR, TB extends T>(options?: RequestInit) => (body: TR) => sendRequest<TR, TB>(rootURL, 'PATCH', body, options),
+        }
     }
 }
+
+export default createEndpoint;
