4c48472655954f122e52681eeea379ba923f44b7
Connor Low <clow2@my.gcu.edu>
2020-02-29 12:54:56 -0800

    Moved ts output to dist/
    Started building factory methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 export * from './client';
+export { default as tysonClientFactory } from './client'
+import createFactory, { RequestInitializerFactory, ResponseMiddleware } from './client';
+
+export default <T>(rootURL: string, init: RequestInitializerFactory, middleware: ResponseMiddleware<T>) => {
+    const sendRequest = createFactory(rootURL, init, middleware);
+    return {
+        createGet: <T>(query: T, options?: RequestInit) => sendRequest(rootURL, 'GET', query, options),
+    }
+}
